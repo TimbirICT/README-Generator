833958dbe9b88e5360c8e09574b27c6bd055e3da
TimbirICT <tmiddlebrooks99@gmail.com>
2023-11-15 18:40:29 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+// TODO: Create a function to initialize app
+// TODO: Create a function to write README file
+// TODO: Create an array of questions for user input
+// Function call to initialize app
+
+const questions = [];
+
+function generateReadMe (data) {
+
+
+
+}
+
+
+function writeToFile(fileName, data) {}
+
+function init() {}
+
+init();
