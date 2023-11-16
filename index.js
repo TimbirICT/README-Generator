ee488dec86fbd11b8c2e24311597741ee0dfc13c
TimbirICT <tmiddlebrooks99@gmail.com>
2023-11-15 19:59:08 -0600

    added inputs and writetofile function

--- index.js
+++ index.js
@@ -3,17 +3,95 @@
 // TODO: Create a function to write README file
 // TODO: Create an array of questions for user input
 // Function call to initialize app
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-const questions = [];
+async function promptUser() {
+    const responses = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your app name?',
+        },
+        {
+            type: 'input',
+            name: 'description', 
+            message: 'Enter description:',
+        },
+        {
+            type: 'input',
+            name: 'Table of Contents', 
+            message: 'List table of contents:',
+        },
+        {
+            type: 'input',
+            name: 'installation', 
+            message: 'Enter Installation Instructions:',
+        },
+        {
+            type: 'input',
+            name: 'usage', 
+            message: 'Enter Usage:',
+        },
+        {
+            type: 'input',
+            name: 'Contributing', 
+            message: 'Enter Contributors:',
+        },
+        {
+            type: 'input',
+            name: 'tests', 
+            message: 'Enter Tests:',
+        },
+        {
+            type: 'input',
+            name: 'questions', 
+            message: 'Enter Questions:',
+        }
+    ]);
+
+    return responses;
+}
+
+
+
 
 function generateReadMe (data) {
+     return `
+##${data.title}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
 
 
+## Contributing
+${data.contributing}
+
+## License
+${data.license}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.questions}
+
+`;
 
 }
 
 
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, readmeContent) {
+    fs.writeFile(fileName, readmeContent, (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+}
 
 function init() {}
 
